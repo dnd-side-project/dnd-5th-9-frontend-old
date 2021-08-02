330b756e0db3266dd5f03e08d16fa20d04cc448c
seokzin <seokzin95@gmail.com>
2021-08-02 16:28:49 +0900

    feat: Make progress bar on the top

--- src/components/pages/Join.jsx
+++ src/components/pages/Join.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import Progress from "./../ui/organisms/Progress";
 
 const Join = () => {
   return (
-    <div>
-      <h1>Join</h1>
+    <>
+      <Progress done="30" />
+      <div>
+        <h1>이메일로 회원가입하기</h1>
 
-      <form action="">
-        <div key="">
-          <p>
-            <label>Username</label>
-          </p>
-          <p>
-            <input name="username" />
-          </p>
-        </div>
-      </form>
-    </div>
+        <form action="">
+          <div key="">
+            <p>
+              <label>Username</label>
+            </p>
+            <p>
+              <input name="username" />
+            </p>
+          </div>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
