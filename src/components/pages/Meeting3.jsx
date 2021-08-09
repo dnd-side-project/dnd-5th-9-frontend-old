75c6c7b2c7fe9e4b5fab2b9624f649d96959de76
seokzin <seokzin95@gmail.com>
2021-08-09 23:53:59 +0900

    feat: Merge map search result code to place page
    - 기존 장소 검색결과창에 지도 출력 기능을 연결하였음

--- src/components/pages/Meeting3.jsx
+++ src/components/pages/Meeting3.jsx
@@ -27,32 +27,35 @@ const Meeting3 = () => {
           검색해주세요.
         </h1>
 
-        <div class="relative text-gray-400 pt-8">
-          <span class="absolute right-0 flex items-center p-1">
-            <Link
-              to="/meeting3"
-              type="submit"
-              class="p-1 focus:outline-none focus:shadow-outline"
-            >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                class="w-6 h-6"
+        <form className="inputForm" onSubmit={handleSubmit}>
+          <div class="relative text-gray-400 pt-8">
+            <span class="absolute right-0 flex items-center p-1">
+              <button
+                type="submit"
+                class="p-1 focus:outline-none focus:shadow-outline"
               >
-                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-              </svg>
-            </Link>
-          </span>
-          <input
-            className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-300 border rounded-lg focus:shadow-outline"
-            name="email"
-            placeholder="장소, 지하철, 주소를 검색해주세요."
-          />
-        </div>
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  class="w-6 h-6"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+              </button>
+            </span>
+            <input
+              className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-300 border rounded-lg focus:shadow-outline"
+              name="email"
+              placeholder="장소, 지하철, 주소를 검색해주세요."
+              onChange={onChange}
+              value={inputText}
+            />
+          </div>
+        </form>
       </div>
 
       <div className="mt-4 mb-4 border-4 border-gray-100"></div>
@@ -72,6 +75,7 @@ const Meeting3 = () => {
       <div>
         <div className="grid grid-cols-8">
           <FaSubway className="m-3 mx-5 col-span-1 text-gray-400 text-xl" />
+
           <div className="col-span-5">
             <p className="">언주역 9호선 3번 출구</p>
             <p className="text-gray-300 text-sm">서울 강남구 논현로 636</p>
@@ -84,18 +88,10 @@ const Meeting3 = () => {
       <p className="text-center text-sm text-gray-300">검색 히스토리 삭제</p>
       <hr className="mt-4" />
 
-      <div className="flex justify-center items-center h-96 text-center text-gray-300">
+      {/* <div className="flex justify-center items-center h-96 text-center text-gray-300">
         이전 검색 기록이 없어요
-      </div>
+      </div> */}
 
-      <form className="inputForm" onSubmit={handleSubmit}>
-        <input
-          placeholder="Search Place..."
-          onChange={onChange}
-          value={inputText}
-        />
-        <button type="submit">검색</button>
-      </form>
       <MapSearchResult searchPlace={place} />
     </>
   );
