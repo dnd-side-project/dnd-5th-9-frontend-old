0ee59a9280136ed06d831a5f016e6a4ddbf26037
GHooN99 <whiteblue0316@gmail.com>
2021-08-14 12:57:40 +0900

    feat: Add CreateForm data

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -9,7 +9,35 @@ const Create = () => {
   const { startDateTimeStamp, endDateTimeStamp } = location.state;
   console.log(location.state);
 
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h1>
+        모임 정보를
+        <br />
+        입력해주세요.
+      </h1>
+
+      <form>
+        <input type="text" name="" id="" placeholder="닉네임을 입력해주세요." />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="모임 이름을 입력해주세요."
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="모임 설명을 입력해주세요."
+        />
+        <input type="time" name="" id="" />
+        <input type="time" name="" id="" />
+        <input type="button" value="" />
+        <input type="button" value="" />
+      </form>
+    </div>
+  );
 };
 
 export default Create;
---
+++ src/components/ui/organisms/CreateForm.jsx
@@ -0,0 +1,25 @@
+const CreateForm = [
+  {
+    label: "닉네임",
+    name: "nickname",
+    type: "text",
+    placeholder: "닉네임을 입력해주세요.",
+    message: "모임에서 사용할 닉네임을 5자 이내로 입력해주세요.",
+  },
+  {
+    label: "모임 이름",
+    name: "meeting-name",
+    type: "text",
+    placeholder: "모임 이름을 입력해주세요.",
+    message: "모임 이름을 10자 이내로 입력해주세요.",
+  },
+  {
+    label: "모임설명",
+    name: "meeting-description",
+    type: "text",
+    placeholder: "모임 설명을 입력해주세요.",
+    message: "모임 설명을 15자 이내로 입력해주세요.",
+  },
+];
+
+export default CreateForm;
