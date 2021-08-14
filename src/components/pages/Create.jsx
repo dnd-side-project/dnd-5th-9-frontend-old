3df5df8cbbfe4ba2b618704cc7ea6e516d0a9b88
GHooN99 <whiteblue0316@gmail.com>
2021-08-14 14:49:21 +0900

    feat: Finish Create Meeting Form layout

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -1,10 +1,20 @@
+import TimePicker from "rc-time-picker";
+import "rc-time-picker/assets/index.css";
+import moment from "moment";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import CreateForm from "../ui/organisms/CreateForm";
+import BottomButton from "../ui/organisms/BottomButton";
 
 const Create = () => {
   const location = useLocation();
   const { startDateTimeStamp, endDateTimeStamp } = location.state;
+  const [isSelected, setIsSelected] = useState(true);
+  const [isDone, setIsDone] = useState(false); // is ?? Done ?
+  const handlePlaceClick = () => {
+    setIsSelected(!isSelected);
+    setIsDone(!isDone);
+  };
   console.log(location.state);
   return (
     <div className="m-4">
@@ -14,7 +24,7 @@ const Create = () => {
         입력해주세요.
       </h1>
 
-      <form>
+      <form onSubmit={(event) => event.preventDefault()}>
         {CreateForm.map((item, index) => {
           return (
             <div key={index}>
@@ -36,11 +46,50 @@ const Create = () => {
             </div>
           );
         })}
-        <input type="time" name="" id="" step="3600" />
-        <input type="time" name="" id="" />
-        <input type="button" value="" />
-        <input type="button" value="" />
+        <div className="block">
+          <label className="font-bold block my-5">모임 시간</label>
+          <div className="">
+            <div className="w-full p-2 flex justify-between border-2 border-gray-400 items-center">
+              <label className="font-bold block">시작 시간</label>
+              <div className="flex items-center justify-center">
+                <TimePicker showSecond={false} minuteStep={30} />
+                <span className="ml-1"> 부터</span>
+              </div>
+            </div>
+            <div className="w-full p-2 flex justify-between border-2 border-gray-400 items-center">
+              <label className="font-bold block">종료 시간</label>
+              <div className="flex items-center justify-center">
+                <TimePicker showSecond={false} minuteStep={30} />
+                <span className="ml-1"> 까지</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <label className="font-bold block my-5"> 중간 장소 추천</label>
+          <div className="flex w-full border-2 border-gray-300">
+            <button
+              className={`flex items-center w-1/2 border-2 border-gray-300 justify-center p-5 ${
+                isSelected && "border-blue-400 text-blue-400"
+              }`}
+              onClick={handlePlaceClick}
+            >
+              필요해요
+            </button>
+            <button
+              className={`flex items-center w-1/2 border-2 border-gray-300 justify-center p-5 ${
+                isSelected && "border-blue-400 text-blue-400"
+              }`}
+              onClick={handlePlaceClick}
+            >
+              다음에 받을래요
+            </button>
+          </div>
+        </div>
       </form>
+      {console.log(isDone)}
+      {isDone && <BottomButton>선택 완료</BottomButton>}
     </div>
   );
 };
